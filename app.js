0cd21a7f3f7357576cac075a0baf7acec6b40639
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-11 15:56:35 -0700

    added autobiography

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // import functions and grab DOM elements
+const labQuizButton = document.getElementById('quizButton');
+console.log(labQuizButton);
 
+const userInputField = document.getElementById('userInput');
+console.log(userInputField);
 // initialize state
 
 // set event listeners to update state and DOM

15b9de3981007704cea68bc6af513b15d3d33e7d
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-11 19:16:10 -0700

    added refactor in utils.js and textContent string

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // import functions and grab DOM elements
+import { countAsAYes } from './utils.js';
+
 const labQuizButton = document.getElementById('quizButton');
 const userInputField = document.getElementById('userInput');
 console.log(userInputField);
@@ -12,37 +14,47 @@ labQuizButton.addEventListener('click', () => {
     if (!confirmation) return; 
 
     const firstName = prompt('What is your first name?');
-    console.log(firstName);
+    // console.log(firstName);
     
     const lastName = prompt('What is your last name?');
-    console.log(lastName);
+    // console.log(lastName);
     
     const firstAnswer = prompt('Was Cierra born in Colorado Springs?');
-    if (firstAnswer.charAt(0).toUpperCase() !== 'Y') {
-        console.log('Perfect!');
-    } else {
-        console.log('Not quite!');
-    }
+    let correctAnswers = 0;
+
+    if (!countAsAYes(firstAnswer)) correctAnswers++;
+    // if (firstAnswer.charAt(0).toUpperCase() !== 'Y') {
+    //     console.log('Perfect!');
+    // } else {
+    //     console.log('Not quite!');
+    // }
 
     const secondAnswer = prompt('Was lacrosse one of the sports she played?');
-    if (secondAnswer.charAt(0).toUpperCase() === 'Y') {
-        console.log('Perfect!');
-    } else {
-        console.log('Not quite!');
-    }
-
+    if (countAsAYes(secondAnswer)) correctAnswers++;
+    // if (secondAnswer.charAt(0).toUpperCase() === 'Y') {
+    //     console.log('Perfect!');
+    // } else {
+    //     console.log('Not quite!');
+    
     const thirdAnswer = prompt('Did Cierra receive a minor in pre-law?');
-    if (thirdAnswer.charAt(0).toUpperCase() === 'Y') {
-        console.log('Perfect!');
-    } else {
-        console.log('Not quite!');
-    }
+    if (countAsAYes(thirdAnswer)) correctAnswers++;
+
+    // if (thirdAnswer.charAt(0).toUpperCase() === 'Y') {
+    //     console.log('Perfect!');
+    // } else {
+    //     console.log('Not quite!');
+    // }
 
     const fourthAnswer = prompt('Is Cierra an extrovert?');
-    if (fourthAnswer.charAt(0).toUpperCase() !== 'Y') {
-        console.log('Perfect!');
-    } else {
-        console.log('Not quite!');
-    }
+    if (!countAsAYes(fourthAnswer)) correctAnswers++;
+    // if (fourthAnswer.charAt(0).toUpperCase() !== 'Y') {
+    //     console.log('Perfect!');
+    // } else {
+    //     console.log('Not quite!');
+    // }
 
+    const resultString = `Hey ${firstName} ${lastName}! You got ${correctAnswers} correct!  Awesome Job!`;
+    console.log(resultString);
+
+    userInputField.textContent = resultString;
 });
